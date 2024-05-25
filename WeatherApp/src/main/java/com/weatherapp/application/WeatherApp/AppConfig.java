c10761a46cbc12ab570051b30ef0d1e19a39bf71
mohsinfarhat96 <mohsinfarhat96@gmail.com>
2024-05-25 11:09:28 -0500

    imp: removed unecessary import statements.

--- WeatherApp/src/main/java/com/weatherapp/application/WeatherApp/AppConfig.java
+++ WeatherApp/src/main/java/com/weatherapp/application/WeatherApp/AppConfig.java
@@ -2,7 +2,6 @@ package com.weatherapp.application.WeatherApp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
--- WeatherApp/src/main/java/com/weatherapp/application/WeatherApp/controller/WeatherController.java
+++ WeatherApp/src/main/java/com/weatherapp/application/WeatherApp/controller/WeatherController.java
@@ -1,14 +1,11 @@
 package com.weatherapp.application.WeatherApp.controller;
 
-
-import com.weatherapp.application.WeatherApp.model.WeatherResponse;
 import com.weatherapp.application.WeatherApp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.http.ResponseEntity;
+
 
 import java.util.Map;
 
