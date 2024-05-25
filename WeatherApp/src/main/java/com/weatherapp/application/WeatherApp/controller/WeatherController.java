bd5f5f9a4cc1f41853a083fb02a23a99f1dd56f9
mohsinfarhat96 <mohsinfarhat96@gmail.com>
2024-05-24 21:10:36 -0500

    imp: modified the service layer to include the business logic (transformation of data)

--- WeatherApp/src/main/java/com/weatherapp/application/WeatherApp/controller/WeatherController.java
+++ WeatherApp/src/main/java/com/weatherapp/application/WeatherApp/controller/WeatherController.java
@@ -1,29 +1,33 @@
 package com.weatherapp.application.WeatherApp.controller;
 
 
-import com.weatherapp.application.WeatherApp.model.ShortWeatherResponse;
 import com.weatherapp.application.WeatherApp.model.WeatherResponse;
 import com.weatherapp.application.WeatherApp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
 
 @RestController
 public class WeatherController {
 
+    //This is considered a dependency injection, since we'll be reusing this service. We shouldn't be initializing an
+    //object everytime as we were in the controller method below.
     @Autowired
-    public RestTemplate restTemplate;
+    private WeatherService service;
 
     //Get Method
     //@GetMapping("/weather")  <--- This maps our getWeather() method to HTTP GET requests.
     //This is also an endpoint for our GET method.
     @GetMapping("/weather")
-    public WeatherResponse getWeather(@RequestParam String city){
-        WeatherService service =  new WeatherService();
-        WeatherResponse response = service.getWeather(city);
+    public Map<String, String> getWeather(@RequestParam String city){
+
+        Map<String,String> response = service.getWeather(city);
         return response;
     }
 }
--- WeatherApp/src/main/java/com/weatherapp/application/WeatherApp/service/WeatherService.java
+++ WeatherApp/src/main/java/com/weatherapp/application/WeatherApp/service/WeatherService.java
@@ -1,25 +1,47 @@
 package com.weatherapp.application.WeatherApp.service;
 
 import com.weatherapp.application.WeatherApp.model.WeatherResponse;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class WeatherService {
-    private final String API_URL = "http://api.weatherstack.com/current?access_key={key}&query={city}";
-    private final String API_KEY = "030f86f222b003ec84a60d5a2901b1c5";
+    //Dependency injections from application.properties.
+    @Value("${weather.api.url}")
+    private  String API_URL;
+
+    @Value("${weather.api.key}")
+    private String API_KEY;
 
     @Autowired
-    private final RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate;
 
-
-    public WeatherResponse getWeather(String city){
+    //Actual call to service being made
+    public Map<String, String> getWeather(String city){
         String url = API_URL.replace("{key}",API_KEY).replace("{city}",city);
         ResponseEntity<WeatherResponse> response = restTemplate.getForEntity(url,WeatherResponse.class);
-        return response.getBody();
+
+        return transformResponse(response.getBody());
+
+
+    }
+    //Transforming the response to the data only we need.
+    public Map<String,String> transformResponse(WeatherResponse response){
+        String cityName = response.getLocation().getName();
+        String localTime = response.getLocation().getLocaltime();
+        String temperature = Integer.toString(response.getCurrent().getTemperature());
+        Map<String,String> map = new HashMap<>();
+        map.put("city",cityName);
+        map.put("time",localTime);
+        map.put("temperature",temperature);
+        return map;
     }
 
 }
