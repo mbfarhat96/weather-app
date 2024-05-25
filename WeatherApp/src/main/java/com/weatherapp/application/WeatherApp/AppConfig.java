fe0a767866936b35349bcf0f8c98db1debe0690c
mohsinfarhat96 <mohsinfarhat96@gmail.com>
2024-05-25 11:02:56 -0500

    fix: encountered major issue that was building build failure, fixed by clearing IDE caches!

--- WeatherApp/src/main/java/com/weatherapp/application/WeatherApp/AppConfig.java
+++ WeatherApp/src/main/java/com/weatherapp/application/WeatherApp/AppConfig.java
@@ -2,6 +2,7 @@ package com.weatherapp.application.WeatherApp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
